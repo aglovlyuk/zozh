7ac865c0269928ecb85334d4d04e64bbdddc3580
Glovliuk Oleksii <aglovlyuk@gmail.com>
2019-12-31 04:53:26 +0200

    Development in progress: countdown + PP

---
+++ src/js/countdown.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import FlipClock from './flipclock.min';
+
+$(function() {
+    var clock;
+
+    $('.js-countdown').each(function () {
+        var currentDate = new Date();
+        var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
+        var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+        clock = $(this).FlipClock(diff, {
+            clockFace: 'HourlyCounter',
+            autoStart: true,
+            countdown: true,
+            language: 'ru'
+        });
+    })
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,5 @@
-import popper from 'popper.js';
+//import popper from 'popper.js';
 import collapse from 'bootstrap/js/dist/collapse';
 
 import './sliders';
-
-if(typeof($.fn.collapse) != 'undefined'){
-    console.log($.fn.collapse)
-} else {
-    console.log('asdf')
-}
+import './countdown';
