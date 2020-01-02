f6598ed6777347d49226ba1108cf5b0d68d3b2c4
Glovliuk Oleksii <aglovlyuk@gmail.com>
2020-01-02 16:30:50 +0200

    Mobile MQ fixes

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
-import FlipClock from './flipclock.min';
+import FlipClock from './libs/flipclock.min';
 
 $(function() {
     var clock;
 
     $('.js-countdown').each(function () {
         var currentDate = new Date();
-        var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
+        var futureDate  = new Date(currentDate.getFullYear(), 0, 3);
         var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
         clock = $(this).FlipClock(diff, {
